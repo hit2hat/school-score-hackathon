8bd0b2ca3209b146f4784c135a6f14a04ec49db8
hit2hat <hit2hat@icloud.com>
2019-10-23 12:18:11 +0300

    Updates

---
+++ precache-manifest.9bb3be6878f88afcec3b9b4557578b46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a328ebe711a029bba900685d176231",
+    "url": "/school-score-hackathon/index.html"
+  },
+  {
+    "revision": "bf404e1ea95a07f83c0e",
+    "url": "/school-score-hackathon/static/css/2.7214840c.chunk.css"
+  },
+  {
+    "revision": "bf404e1ea95a07f83c0e",
+    "url": "/school-score-hackathon/static/js/2.34d1fcb0.chunk.js"
+  },
+  {
+    "revision": "ca886ac8a30b5ec2c533",
+    "url": "/school-score-hackathon/static/js/main.c42d064b.chunk.js"
+  },
+  {
+    "revision": "54fc2c5666717fade3e1",
+    "url": "/school-score-hackathon/static/js/runtime-main.a968a958.js"
+  }
+]);
